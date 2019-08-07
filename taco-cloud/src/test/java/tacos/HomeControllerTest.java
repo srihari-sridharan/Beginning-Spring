2689e03f6da93721aabf14fd433a4740b20c1e08
Srihari Sridharan <srihari.sridharan.r@gmail.com>
2019-08-07 19:10:26 +0530

    Fixed failing unit tests

--- taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
+	@Autowired
 	private MockMvc mockMvc;
 	
 	@Test
